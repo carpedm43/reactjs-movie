7b8a6bf0f46fdb3cfb6b1390b7fd702231ccccf4
vividsunmi <carpedm43@gmail.com>
2021-03-16 00:24:18 +0900

    add life cycle

--- src/App.js
+++ src/App.js
@@ -12,7 +12,20 @@ class App extends React.Component {
         this.setState(current_state => ({count: current_state.count - 1}))
     };
 
+    componentDidMount() {
+        console.log("rendered")
+    }
+
+    componentDidUpdate () {
+        console.log("update")
+    }
+
+    componentWillUnmount() {
+        console.log("Goodbye, cruel world")
+    }
+
     render () {
+        console.log("i'm rendering");
         return (
             <div>
                 <h1>The number is: {this.state.count}</h1>
