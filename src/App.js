7a1b8caacdbca5e8c78b14c654e643ca329ab68a
vividsunmi <carpedm43@gmail.com>
2021-03-16 22:49:19 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
     state = {
-        count: 0
+        isLoading: true,
+        movies: []
+    };
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies }
+            }
+        } = await axios.get(
+            "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+        );
+        this.setState({ movies, isLoading: false });
     };
 
-    add = () => {
-        this.setState(current_state => ({count: current_state.count + 1}))
-    };
-    minus = () => {
-        this.setState(current_state => ({count: current_state.count - 1}))
-    };
+    componentDidMount () {
+        this.getMovies();
 
-    componentDidMount() {
-        console.log("rendered")
     }
-
-    componentDidUpdate () {
-        console.log("update")
-    }
-
-    componentWillUnmount() {
-        console.log("Goodbye, cruel world")
-    }
-
     render () {
-        console.log("i'm rendering");
+        const { isLoading, movies } = this.state;
         return (
-            <div>
-                <h1>The number is: {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
-            </div>
-        )
+            <section className="container">
+            {isLoading ? (
+                <div className="loader">
+                    <span className="loader__text">Loading...</span>
+                </div>
+            ) : (
+                <div className="movies">
+                    {movies.map(movie => (
+                        <Movie
+                            key={movie.id}
+                            id={movie.id}
+                            year={movie.year}
+                            title={movie.title}
+                            summary={movie.summary}
+                            poster = {movie.medium_cover_image}
+                        />
+                    ))}
+                        </div>
+                        )}
+                    </section>
+        );
     }
 }
-
-
 export default App;
